b0d18435c6b482085dbf51e23d940e7c16d858bb
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-07-26 09:55:27 -0400

    commit WROKING

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,13 @@
 const playerHealthBar = document.getElementById('player-health');
 const monsterHealthBar = document.getElementById('monster-health');
+let monsterStrongAttact = true;
+let monsterHeal = true;
 let mHealth = 100;
 let pHealth = 100;
-
+let roundNumber = 0;
 
 export function round(modifier) {
-  
+  roundNumber += 1
   if (mHealth === 0 && pHealth === 0) {
     console.log('Its a tie game');
     return;
@@ -20,13 +22,20 @@ export function round(modifier) {
 
   let attack = (modifier) => {
     if (modifier === '+') {
-      pHealth += damage(10);
-  
+      pHealth += damage(20);
     } else {
       mHealth -= damage(modifier);
     }
+    if (mHealth < 50 && monsterHeal) {
+      mHealth += damage(20);
+      monsterHeal = false;
+    } else if (mHealth < 30 && monsterStrongAttact ) {
+      pHealth -= damage(20);
+      monsterStrongAttact = false;
+    } else {
+      pHealth -= damage(10);
+    }
 
-    pHealth -= damage(10);
     monsterHealthBar.value = mHealth;
     playerHealthBar.value = pHealth;
   };
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,7 +1,5 @@
 import { round } from './app.js';
 
-
-
 const bonusLifeEl = document.getElementById('bonus-life');
 
 const attackBtn = document.getElementById('attack-btn');
@@ -10,12 +8,14 @@ const healBtn = document.getElementById('heal-btn');
 const logBtn = document.getElementById('log-btn');
 const consoleLog = document.getElementById('console-Log');
 
-attackBtn.addEventListener('click', () => round.call(this, 10));
-strongAttackBtn.addEventListener('click', () => round.call(this, 90));
+attackBtn.addEventListener('click', function () {
+  round.call(this, 10);
+});
+strongAttackBtn.addEventListener('click', () => {
+  strongAttackBtn.disabled = true;
+  round.call(this, 20);
+});
 healBtn.addEventListener('click', () => {
-    healBtn.disabled = true
+  healBtn.disabled = true;
   round.call(this, '+');
 });
-
-
-
