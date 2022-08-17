8eac1eae225471502dd6b25c893186b53a244f79
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-08-17 11:47:39 -0400

    commit

--- assets/scripts/simpleGenerator.js
+++ assets/scripts/simpleGenerator.js
@@ -1,10 +1,32 @@
-
-let id  = 1
-export function* simpleGenerator() {
-   while (true){
-    yield id    
-    id++
-   }
+let wolfObj = {
+    pickup: 'Chevy Silverado',
+    address: `16716 lola`,
+    legelAge: 16,
+    ['peanut butter']: 'all natural',
+    ['plane ticket']: 'CUBA',
+    car: 'fictional car',
+    prop3: !undefined,
+    johnny5: 'JOHNNY5',
+    legalAgeMeth(param1) {
+      switch (true) {
+        case this.legelAge <= param1:
+          return true;
+      }
+    },
+    age: 47,
+    profession: 'Computer programming',
+    Hobbies: 'swimming',
+  };
  
-    return 'banana'
+export function* simpleGenerator(array,) {
+ let size = Object.keys(wolfObj).length
+ 
+  for (const key in wolfObj) {
+   let wolf = yield [key, size];
+   
+    size = size - wolf
+    console.log(`${size}: ${key}: ${wolfObj[key]}`)
+  }
+
+  return 'banana';
 }
