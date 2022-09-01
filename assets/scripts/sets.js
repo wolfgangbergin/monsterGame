548459121511ee0ac0ed790653a24930dfe467bf
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-09-01 10:19:23 -0400

    return this and arrow functions

--- assets/scripts/sets.js
+++ assets/scripts/sets.js
@@ -1,32 +1,49 @@
-const WOLF = 'wolfMan' || (() => 0)();
-const FALSE = false;
-const TESTMICH = FALSE || (WOLF && (() => 'kim22')());
-console.log(`TESTMICH: ${TESTMICH}`);
 
-// const JOBOB = WOLF && 100;
+//arrow functions leave the best keyword Alone
+class User1 {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+    this.score = 0;
+  }
+  login() {
+    console.log(`${this.name} logedin!`);
+    return this;
+  }
+  logout() {
+    console.log(`${this.name} logedout!`);
+    return this;
+  }
+  addScore() {
+    this.score++;
+    console.log(`the score is ${this.score}!`);
+    return this;
+  }
+  subScore = () => {
+    this.score--;
+    console.log(`the score is ${this.score}!`);
+    return this;
+  };
+}
 
-// switch (true) {
-//   case (KIM && WOLF === 'wolf') || JOBOB > 99:
-//     console.log(`&& || Operator`);
-//     break;
-//   case WOLF === 'wolf':
-//     console.log('wolf');
-//     break;
-//   case WOLF === '':
-//     console.log('empty string!');
-//     break;
-//   case WOLF === true:
-//     console.log('true');
-//     break;
-//   case WOLF === false:
-//     console.log('false');
-//     break;
-//   case WOLF === undefined:
-//     console.log('undefined');
-//     break;
-//   default:
-//     console.log('default');
-//     break;
-// }
+class User2 {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+    this.score = 0;
+  }
+  login = wolfman.login;
+  logout = wolfman.logout;
+  addScore = wolfman.addScore;
+  subScore = wolfman.subScore;
+}
+
+
+var wolfman = new User1('wolfman', 'wolfman@gmial.com');
+var kim = new User2('kim22', 'kim@gmial.com');
+
+console.log(
+  kim.login().logout().addScore().addScore().addScore().subScore().subScore()
+);
 
 export function jobob(...props) {}
